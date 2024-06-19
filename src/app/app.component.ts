ce79bde1bc50bd8a9ee542793d52ad23ed0aed9d
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-06-18 22:16:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { CommonModule, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'teste';
+
 }
