0d39ba4f091210a2d0d4577af03ab926cd5071f2
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-07-13 12:10:30 -0300

    fix: change name from value to price on DTOs

--- backend/src/main/java/com/cardapio/backend/DTO/request/RequestProductDTO.java
+++ backend/src/main/java/com/cardapio/backend/DTO/request/RequestProductDTO.java
@@ -5,7 +5,7 @@ import com.cardapio.backend.models.Category;
 public record RequestProductDTO(
     String id,
     String name,
-    float value,
+    float price,
     String description,
     int quantity,
     String urlImage,
