f8526ba46a7a018775c4a5e4f3ae07e59c03080a
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-07-16 19:40:15 -0300

    fix: change table name

--- backend/src/main/java/com/cardapio/backend/models/Order.java
+++ backend/src/main/java/com/cardapio/backend/models/Order.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "tb_order")
 public class Order {
     
     @Id
