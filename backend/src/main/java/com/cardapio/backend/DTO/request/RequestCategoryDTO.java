6a8142f41d94e11b5b8feca60a779909e823906e
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-07-16 19:09:30 -0300

    fix: create MultipartFile attribute

--- backend/src/main/java/com/cardapio/backend/DTO/request/RequestCategoryDTO.java
+++ backend/src/main/java/com/cardapio/backend/DTO/request/RequestCategoryDTO.java
@@ -1,8 +1,10 @@
 package com.cardapio.backend.DTO.request;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public record RequestCategoryDTO(
     String id,
-    String urlImage,
+    MultipartFile image,
     String description
 ) {
     
