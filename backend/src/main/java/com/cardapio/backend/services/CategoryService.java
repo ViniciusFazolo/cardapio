b7274732dba55d59c98d50b6b9a0c70b5976a2a3
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-07-22 14:19:06 -0300

    fix: refact

--- backend/src/main/java/com/cardapio/backend/services/CategoryService.java
+++ backend/src/main/java/com/cardapio/backend/services/CategoryService.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
