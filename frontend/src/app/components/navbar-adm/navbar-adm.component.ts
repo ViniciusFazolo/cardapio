a62e424a42faab8c2169a353acc468ffb9ff4ab5
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-07-26 11:28:49 -0300

    feat: logou funcionalidade

--- frontend/src/app/components/navbar-adm/navbar-adm.component.ts
+++ frontend/src/app/components/navbar-adm/navbar-adm.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router, RouterLinkWithHref } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar-adm',
