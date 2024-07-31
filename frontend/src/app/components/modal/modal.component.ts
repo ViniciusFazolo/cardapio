0d11a2c9ec903132acf726945bddd17570dcaed7
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-07-30 21:40:27 -0300

    feat: componente de modal

---
+++ frontend/src/app/components/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  @Input() id!: string;
+  @Input() title!: string;
+  @Input() btnPrimary!: string;
+  @Input() btnSecondary!: string;
+  @Input() positionMiddle: boolean = false;
+  @Input() scrollable: boolean = false;
+  @Output() confirm = new EventEmitter();
+
+  onClick() {
+    this.confirm.emit();
+  }
+}
