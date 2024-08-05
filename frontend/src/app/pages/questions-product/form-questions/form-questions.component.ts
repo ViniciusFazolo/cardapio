2dfba5f6d8683ebd46341824e87b2ee358c73a6c
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-08-05 11:59:42 -0300

    feat: crud de coleção de perguntas

--- frontend/src/app/pages/questions-product/form-questions/form-questions.component.ts
+++ frontend/src/app/pages/questions-product/form-questions/form-questions.component.ts
@@ -1,31 +1,150 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DefaultLayoutPagesComponent } from '../../../components/default-layout-pages/default-layout-pages.component';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { BtnsEndComponent } from '../../../components/btns-end/btns-end.component';
 import { InputGroupModule } from 'primeng/inputgroup';
+import {
+  Option,
+  ProductOption,
+  ProductOptionService,
+} from '../../../services/product-option.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-questions',
   standalone: true,
-  imports: [DefaultLayoutPagesComponent, ReactiveFormsModule, BtnsEndComponent, InputGroupModule],
+  imports: [
+    DefaultLayoutPagesComponent,
+    ReactiveFormsModule,
+    BtnsEndComponent,
+    InputGroupModule,
+  ],
   templateUrl: './form-questions.component.html',
-  styleUrl: './form-questions.component.css'
+  styleUrl: './form-questions.component.css',
 })
-export class FormQuestionsComponent {
-  myForm: FormGroup
+export class FormQuestionsComponent implements OnInit {
+  id!: string | null;
+  myForm: FormGroup;
 
-  constructor(){
+  showSkeleton: boolean = false;
+
+  constructor(
+    private productOptionService: ProductOptionService,
+    private toastr: ToastrService,
+    private activatedRoute: ActivatedRoute,
+    private route: Router
+  ) {
     this.myForm = new FormGroup({
+      required: new FormControl(true),
       description: new FormControl('', [Validators.required]),
-      questions: new FormArray([])
+      options: new FormArray([]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.id = params.get('id');
+      if (this.id) {
+        this.showSkeleton = true;
+        this.productOptionService.getById(this.id).subscribe(
+          () => {
+            this.getById();
+          },
+          () => {
+            this.route.navigate(['/adm/productOption']);
+          }
+        );
+      }
+    });
+  }
+
+  save(): void {
+    if (!this.myForm.valid) {
+      this.toastr.warning('Preencha todos os campos!');
+      return;
+    }
+
+    if (this.id) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  create() {
+    const options: Option[] = (this.myForm.get('options') as FormArray).controls.map(control => ({
+      option: control.value,
+    }));
+
+    const produtOption: ProductOption = {
+      description: this.myForm.value.description,
+      required: this.myForm.value.required,
+      options: options
+    };
+
+    this.productOptionService.create(produtOption).subscribe({
+      next: () => {
+        this.toastr.success('Cadastrado com sucesso!');
+        this.route.navigate(['/adm/productOption']);
+      },
+      error: () => {
+        this.toastr.error('Erro ao cadastrar, tente novamente!');
+      },
+    });
+  }
+
+  update() {
+    const options: Option[] = (this.myForm.get('options') as FormArray).controls.map(control => ({
+      option: control.value,
+    }));
+
+    const produtOption: ProductOption = {
+      id: this.id!,
+      description: this.myForm.value.description,
+      required: this.myForm.value.required,
+      options: options
+    };
+
+    this.productOptionService.update(produtOption).subscribe({
+      next: () => {
+        this.toastr.success('Atualizado com sucesso!');
+        this.route.navigate(['/adm/productOption']);
+      },
+      error: () => {
+        this.toastr.error('Erro ao atualizar, tente novamente!');
+      },
+    });
+  }
+
+  getById() {
+    this.productOptionService.getById(this.id!).subscribe((response) => {
+      this.myForm.patchValue({
+        description: response.description,
+        required: response.required
+      })
+      
+      const optionsArray = this.myForm.get('options') as FormArray;
+      optionsArray.clear();
+      response.options.forEach(option => {
+        optionsArray.push(new FormControl(option.option, [Validators.required]));
+      });
     })
   }
 
-  addQuestion(){
-    (this.myForm.get('questions') as FormArray).push(new FormControl('', [Validators.required]))
+  addQuestion() {
+    (this.myForm.get('options') as FormArray).push(
+      new FormControl('', [Validators.required])
+    );
   }
 
-  removeQuestion(index: number){
-    (this.myForm.get('questions') as FormArray).removeAt(index)
+  removeQuestion(index: number) {
+    (this.myForm.get('options') as FormArray).removeAt(index);
   }
 }
