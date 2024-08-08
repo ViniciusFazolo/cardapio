4bbd7ce2228592dc166b836f920a5172956167c5
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-08-08 09:42:28 -0300

    feat: observações do produto componente

---
+++ frontend/src/app/components/product-notes/product-notes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-notes',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-notes.component.html',
+  styleUrl: './product-notes.component.css'
+})
+export class ProductNotesComponent {
+
+}
