d922fcc9a4ea95524d025315de09330fb83e4fe4
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-08-08 22:04:30 -0300

    feat: mostrando as perguntas do produto no modal

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,20 +1,36 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../pages/home-user/home-user.component';
 import { ModalComponent } from '../modal/modal.component';
+import { ChooseQtdProductComponent } from '../choose-qtd-product/choose-qtd-product.component';
+import { ProductNotesComponent } from '../product-notes/product-notes.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe, ModalComponent],
+  imports: [CurrencyPipe, ModalComponent, ChooseQtdProductComponent, ProductNotesComponent, ReactiveFormsModule, CheckboxModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  myForm: FormGroup
+  
   @Input() product!: Product;
-  @Output() openProductDetails = new EventEmitter<Product>()
 
-  onClick(product: Product){
-    this.openProductDetails.emit(product)
+  constructor(){
+    this.myForm = new FormGroup({
+      qtItems: new FormControl(0, [Validators.required]),
+      notes: new FormControl('')
+    })
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  submit(){
+    console.log(this.myForm.value.notes)
   }
 }
