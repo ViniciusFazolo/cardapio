2887487636da051d62e7a05b454c744c06886c9a
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-08-12 14:30:33 -0300

    feat: spinner numerico

---
+++ frontend/src/app/components/numeric-spinner/numeric-spinner.component.ts
@@ -0,0 +1,56 @@
+import { NgClass } from '@angular/common';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+
+@Component({
+  selector: 'app-numeric-spinner',
+  standalone: true,
+  imports: [NgxMaskDirective, NgxMaskPipe, ReactiveFormsModule, NgClass],
+  templateUrl: './numeric-spinner.component.html',
+  styleUrl: './numeric-spinner.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NumericSpinnerComponent),
+      multi: true,
+    },
+  ]
+})
+export class NumericSpinnerComponent implements ControlValueAccessor{
+  onChange: any = () => {};
+  onTouched: any = () => {};
+  value: number = 0;
+
+  @Input() label: string = ''
+  @Input() class: string = ''
+  
+  add() {
+    if (this.value == 99) return;
+    this.value += 1;
+    this.onChange(this.value)
+  }
+
+  less() {
+    if (this.value == 1) return;
+    this.value -= 1;
+    this.onChange(this.value)
+  }
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+  }
+}
