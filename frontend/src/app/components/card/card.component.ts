a17d3af47559cbceb3886c32fc85d846838a7f5d
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-08-26 09:44:39 -0300

    feat: mostra os produtos ao clicar no card
    feat: mostra os produtos ao clicar no card

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -1,16 +1,22 @@
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Category } from '../../interfaces/category/categoryHome';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  isProductsVisible: boolean = false
+
   @Input() subtitle: string = '';
   @Input() text: string = '';
   @Input() category!: Category
+
+  toggleProductsVisibility(){
+    this.isProductsVisible = !this.isProductsVisible
+  }
 }
